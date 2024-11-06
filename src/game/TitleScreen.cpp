00b088ce550aeec596496d56dc8f48c6d0f080a3
schumaker <hudson.schumaker@me.com>
2024-11-06 18:19:28 +0100

    rock-rain: fix Titlescreen background.

--- src/game/TitleScreen.cpp
+++ src/game/TitleScreen.cpp
@@ -37,91 +37,20 @@ TitleScreen::~TitleScreen() {
 
 void TitleScreen::load() {
 	nextScene = "0";
-	logoTexture = Gfx::getInstance()->loadTexture("dodoi.png");
+	logoTexture = Gfx::getInstance()->loadTexture(std::string(IMAGE_FOLDER) + "title.png");
 	Dimension<int> size = Gfx::getInstance()->getTextureSize(logoTexture);
 	rect = {
-		Defs::SCREEN_H_WIDTH - (size.w / 2) - 20,  // x
-		Defs::SCREEN_H_HEIGHT - (size.h / 2) - 20, // y
-		size.w,                                    // w 
-		size.h                                     // h
+		23, // x
+		0,  // y
+		size.w, // w 
+		size.h  // h
 	};
-
-	//for (int i = 0; i < 100000; i++) {
-	//	auto entity = EntityManager::getInstance()->createEntity(1 + i * 2, 1 + i * 2, Tag::COIN);
-	//	entity->addComponent(new Box(30, 30, false, Color::getWhite()))->isFilled = false;
-	//	entity->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-	//	entity->zIndex = i; // Set z-order
-	//}
-
-	//for (int i = 0; i < 100000; i++) {
-	//	auto entity = EntityManager::getInstance()->createEntity(10 + i * 10, 10 + i * 10, Tag::COIN);
-	//	entity->addComponent(new Box(30, 30, false, Color::getGold()))->isFilled = false;
-	//	entity->layer = Layer::BACKGROUND; // Set layer to BACKGROUND
-	//	entity->zIndex = i; // Set z-order
-	//}
-
-	for (int i = 0; i < 1000; i++) {
-		auto a = EntityManager::getInstance()->createEntity(i, i, Tag::COIN);
-		a->addComponent(new Sprite("avatarShadow"));
-		a->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-		a->addComponent(new RigidBody(32.0f, 32.0f));
-		auto w = a->addComponent(new Waypoint({ 300,456 }));
-		w->addPoint({ 100,600 });
-		w->addPoint({ 500,455 });
-		w->addPoint({ 300,100 });
-
-		auto b = EntityManager::getInstance()->createEntity(i, i, Tag::COIN);
-		b->addComponent(new Box(50, 50, false, Color::getGreen()))->isFilled = true;
-		b->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-		b->addComponent(new RigidBody(18.0f, 18.0f));
-		b->addComponent(new Waypoint({ 500,456 }));
-
-		auto c = EntityManager::getInstance()->createEntity(i, i, Tag::COIN);
-		c->addComponent(new Box(50, 50, false, Color::getBrown()))->isFilled = true;
-		c->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-		c->addComponent(new RigidBody(18, 18));
-		c->addComponent(new Waypoint({ 900,100 }));
-
-		auto d = EntityManager::getInstance()->createEntity(i, i, Tag::COIN);
-		d->addComponent(new Circle(25, false, false, Color::getRed()))->isDashed = true;
-		d->layer = Layer::FOREGROUND; // Set layer to BACKGROUND
-		d->zIndex = 3; // Set z-order
-		d->addComponent(new RigidBody(18.0f, 18.0f));
-		d->addComponent(new Waypoint({ 456,399 }));
-	}
-
-	// Create a new box
-	auto a = EntityManager::getInstance()->createEntity(70, 67, Tag::COIN);
-	a->addComponent(new Box(50, 50, false, Color::getGray()))->isFilled = true;
-	a->layer = Layer::BACKGROUND; // Set layer to BACKGROUND
-	/*a->addComponent(new RigidBody(28.0f, 28.0f));
-	auto waypoint = a->addComponent(new Waypoint({300,456}));
-	waypoint->addPoint({ 100,200 });*/
-
-	auto b = EntityManager::getInstance()->createEntity(55, 55, Tag::COIN);
-	b->addComponent(new Box(50, 50, false, Color::getMagenta()))->isFilled = true;
-	b->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-	b->zIndex = 1; // Set z-order
-	b->addComponent(new RigidBody(18.0f, 18.0f));
-	b->addComponent(new Waypoint({ 500,456 }));
-
-	auto c = EntityManager::getInstance()->createEntity(60, 60, Tag::COIN);
-	c->addComponent(new Box(50, 50, false, Color::getOrange()))->isFilled = true;
-	c->layer = Layer::MIDDLEGROUND; // Set layer to BACKGROUND
-	c->zIndex = 8; // Set z-order
-	c->addComponent(new RigidBody(18, 18));
-	c->addComponent(new Waypoint({ 900,100 }));
-
-	auto d = EntityManager::getInstance()->createEntity(100, 60, Tag::COIN);
-	d->addComponent(new Circle(25, false, false, Color::getPurple()))->isDashed = true;
-	d->layer = Layer::FOREGROUND; // Set layer to BACKGROUND
-	d->zIndex = 3; // Set z-order
-	d->addComponent(new RigidBody(18.0f, 18.0f));
 
 	isRunning = true;
 }
 
 short TitleScreen::run() {
+	load();
 	while (isRunning) {
 		input();
 		update();
@@ -144,21 +73,15 @@ void TitleScreen::input() {
 
 void TitleScreen::update() {
 	float dt = calculateDeltaTime();
-	WaypointNavigationSystem nav;
-	nav.update(dt);
 }
 
 void TitleScreen::render() {
-	beginRender();
-	{
-		//SDL_RenderCopy(renderer, logoTexture, NULL, &rect);
-		PrimitiveRenderSystem render;
-		render.update(&camera);
 
-		RenderSystem renderSystem;
-		renderSystem.update(&camera);
-	}
-	endRender();
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, logoTexture, NULL, &rect);
+	SDL_RenderPresent(renderer);
+
 }
 
 void TitleScreen::unload() {
